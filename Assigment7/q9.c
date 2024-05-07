0a4d16e21e80216c2f658a1112836dc1164df0a8
Kashish Goyal <kashish1782004@gmail.com>
2024-05-07 23:16:28 +0530

    union

--- Assigment7/q9.c
+++ Assigment7/q9.c
@@ -1,2 +1,4 @@
 //  The main differnce between a structure and union is the memory allocation.
 // a.In structure memory is alloacted to individual elemnts ecah elemnt has it's own adress wheareas in union memomry is allocated on to a complete sample/template.
+// Umion is useful in asccesing one memeber onlu and stop memory wastage. wheares in structure all the elments can be asccsed.
+//we don't use union anymore.
