f929ee0e32df206ed7af2f5d2520d4b584e3a0d0
Kashish Goyal <kashish1782004@gmail.com>
2024-05-18 00:33:06 +0530

    updated merged_array.c

--- extras/merging_array.c
+++ extras/merging_array.c
@@ -4,7 +4,6 @@ int main(){
     int size_a1=sizeof(a1)/sizeof(a1[0]);
     int a2[]={6,7,8,9,10};
     int size_a2=sizeof(a2)/sizeof(a2[0]);
-    int a2[]={6,7,8,9,10};
     int merged_size=size_a1+size_a2;
     int merged_a[merged_size];
 
@@ -15,9 +14,9 @@ int main(){
      for(int i=0;i<size_a2;i++){
         merged_a[size_a1+i]=a2[i];
     }
-    
-    for(int i=0;i<merged_size,i++){
-        printf("%d",merged_a);
+
+    for(int i=0;i<merged_size;i++){
+        printf("%d\t",merged_a[i]);
     }
     return 0;
 }
