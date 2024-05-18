d0a7dcfcb3e381c8e40f29181b66fc0456556730
Kashish Goyal <kashish1782004@gmail.com>
2024-05-18 14:28:12 +0530

    question 1

--- Assignment8/q1.c
+++ Assignment8/q1.c
@@ -1,7 +1,21 @@
-#include<stdio.h>
-int main(){
-    FILE *fp;
-    fp = fopen("story.txt","r");
+#include <stdio.h>
+int main()
+{
+    FILE *fp = NULL;
+    char ch;
+    fp = fopen("story.txt", "r");
+    if (fp == NULL)
+    {
+        printf("File doesn't exist");
+    }
+    else
+    {
+        while (!feof(fp))
+        {
+            ch = fgetc(fp);
+            printf("%c", ch);
+        }
+    }
     fclose(fp);
     return 0;
 }
