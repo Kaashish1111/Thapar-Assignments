13ba99cc1a048a2e204c60a35b1f77dfb21c8a9a
Kashish Goyal <kashish1782004@gmail.com>
2024-09-26 18:28:50 +0530

    updated question 10

--- Assignment2/q10.c
+++ Assignment2/q10.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
     char *s = "3.145"; // Initializing a string
-    
+
     // Using type casting functions to convert string to different data types
     double d = atof(s); // Convert string to double
     int i = atoi(s);    // Convert string to integer
@@ -14,6 +15,11 @@ int main() {
     printf("Double: %f\n", d);
     printf("Integer: %d\n", i);
     printf("Long: %ld\n", l);
+
+    char st[20];
+    int a = 123;     // assume a is an integer variable
+    itoa(a, st, 10); // convert a to decimal string and store in st
+    printf("%s",st);
 
     return 0;
 }
